cf2de4b9bd5b88f1a550d5d3b90cd68d94d78e53
Pretty Nonhlanhla Kunene <32060984+nonoza@users.noreply.github.com>
2024-05-06 14:07:05 +0200

    added a popup

--- js/script.js
+++ js/script.js
@@ -8,6 +8,19 @@ window.addEventListener("load", () => {
   let mobileIcon = document.querySelector(".navbar-toggler");
   let mainMenu = document.querySelector(".main-menu");
   let login = document.querySelector(".login");
+  let register = document.querySelector('.btn-outline');
+  const form = document.querySelectorAll('form');
+  const closeX = document.querySelectorAll('.popup-close');
+  const loginPopUp = document.querySelector('.popup-login');
+  const registerPopup = document.querySelector('.popup-register ');
+  const popup = document.querySelector('.popup-wrapper');
+  const loginLink = document.querySelector('.login-link');
+const regsterLink = document.querySelector('.register-link');
+const lastName = document.querySelector('#firstNname');
+const email = document.querySelector('#email-address');
+const password  = document.querySelector('#password');
+const errorMsg = document.querySelectorAll('.error-mgs');
+  
 
   //Dropdown icon switch
   dropDownMenu.forEach((menu, index) => {
@@ -46,4 +59,57 @@ window.addEventListener("load", () => {
 
   //Initialising AOS js library
   AOS.init();
+
+  //script for popup
+  login.addEventListener('click',()=> {
+    popup.style.display = "flex";
+    registerPopup.style.display = "none";
+    loginPopUp.style.display = "block";
+  })
+
+  register.addEventListener('click',()=> {
+    popup.style.display = "flex";
+    registerPopup.style.display = "block";
+    loginPopUp.style.display = "none";
+  })
+
+  closeX.forEach( closebtn => {
+    closebtn.addEventListener('click',()=>{
+        popup.style.display = "none";
+    })
+})
+
+
+loginLink.addEventListener('click',()=>{
+ loginPopUp.style.display = "block";
+ registerPopup.style.display = "none";
 });
+
+regsterLink.addEventListener('click',()=>{
+    loginPopUp.style.display = "none";
+    registerPopup.style.display = "block";
+   })
+
+
+
+form.forEach(form =>{
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('clicked');
+    
+        // Validate the empty values
+        if (lastName.value.trim() === "" && email.value.trim() === "" && password.value.trim() === "" ) { // Trim any leading or trailing whitespace
+            errorMsg[0].textContent = "Enter your details";
+            errorMsg[1].textContent = "Enter your details";
+        } else {
+            // Clear any previous error messages
+            errorMsg[0].textContent = "";
+        }
+    });
+})
+});
+
+
+
+
+
